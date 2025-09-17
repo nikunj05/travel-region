1790123b88c4c3be61e9108bac26995e2c74c77a
Parth <parth.k@coderscotch.com>
2025-09-17 16:49:47 +0530

    Add FaqCategory resource with CRUD pages and form schema

---
+++ app/Filament/Resources/FaqCategories/Schemas/FaqCategoryForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\FaqCategories\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Schema;
+
+class FaqCategoryForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Grid::make()
+                    ->columns(12)
+                    ->columnSpanFull()
+                    ->schema([
+                        TextInput::make('name')
+                            ->required()
+                            ->columnSpan(6),
+                ]),
+            ]);
+    }
+}
