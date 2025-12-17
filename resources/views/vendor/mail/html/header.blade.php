c262c13b1e90a35e4600d79a73dfc90e7f241044
Parth <parth.k@coderscotch.com>
2025-12-17 12:37:20 +0530

    feat: Add email template components including header, footer, button, and layout files

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+<tr>
+    <td class="header">
+        <a href="{{ config('app.url') }}" style="display: inline-block;">
+            <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}"
+                style="height: 40px; max-width: 100%; display: block;">
+        </a>
+    </td>
+</tr>
