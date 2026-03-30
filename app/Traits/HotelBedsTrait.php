0ec3c176a9051852c23e68566f50aad10499052c
Parth <parth.k@coderscotch.com>
2026-03-30 17:19:27 +0530

    Reapply "fix: Update hotel image retrieval to support multiple images per hotel"
    This reverts commit 2cefacdd6794cb7dd92d84bda0ab563fb3b37b0d.

--- app/Traits/HotelBedsTrait.php
+++ app/Traits/HotelBedsTrait.php
@@ -114,8 +114,7 @@ trait HotelBedsTrait
             return DB::table('hotel_images')
                 ->whereIn('hotel_code', $hotelCodes)
                 ->select(['hotel_code', 'path', 'image_type_code'])
-                ->get()
-                ->keyBy('hotel_code');
+                ->get();
         });
 
         // 5. Cache facilities grouped by hotel code
@@ -258,16 +257,16 @@ trait HotelBedsTrait
 
             // O(1) lookups from cached data
             $localHotel      = $localHotelsMap[$code] ?? null;
-            $image           = $firstImages[$code] ?? null;
             $hotelFacilities = $facilitiesByHotel[$code] ?? collect();
 
             $hotel['accommodationTypeCode'] = $localHotel['accommodation_type_code'] ?? null;
             $hotel['address']               = ['content' => $localHotel['address'] ?? null];
             $hotel['city']                  = ['content' => $localHotel['city'] ?? null];
-            $hotel['images']                = [[
-                'path'          => $image->path ?? null,
-                'imageTypeCode' => $image->image_type_code ?? null,
-            ]];
+            $hotel['images']                = collect($firstImages->where('hotel_code', $code)->take(5))->map(fn($i) => [
+                'path' => $i->path ?? null,
+                'imageTypeCode' => $i->image_type_code ?? null,
+                'code' => $i->hotel_code ?? null,
+            ])->values()->toArray();
 
             $hotel['facilities'] = $hotelFacilities->map(fn($f) => [
                 'facilityCode'      => $f->facility_code,
